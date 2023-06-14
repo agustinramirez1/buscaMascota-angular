fe910b773120b9162c8487d612b2c257e749c137
Agustin Ramirez <agusramirez2001@gmail.com>
2023-06-14 17:51:52 -0400

    Se agregó un componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
